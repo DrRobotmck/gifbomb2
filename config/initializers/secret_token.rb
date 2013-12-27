1b3174843d7581f254d2b8530aa6a50eb4e41ee9
DrRobotmck <mckenneth.scott@icloud.com>
2013-12-27 14:32:55 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gifbomb2::Application.config.secret_key_base = '1a7de9075c71a1dab37b58e675593d1a141951a6ad3be6a96da69dd9571152bb57609f18edce91a6e759303860fc5a571e8b7174162c56df825064834919c712'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gifbomb2::Application.config.session_store :cookie_store, key: '_gifbomb2_session'
